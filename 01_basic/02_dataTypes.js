fe742b645990c9c8659e47f8b3b49eaa43ad94b1
dkp3209 <85048902+dkp3209@users.noreply.github.com>
2024-02-01 13:54:02 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all Js code as newer version 
+
+//alert(3+3) // we are using nodejs, not browser...
+ // number => 2 to power 53
+ // bigint 
+ // string =>""
+ // null=>standalone value
+ // undefined 
+ // symbol =>uniqueness
+
+
+ // Object
+
+ 
+ console.log(null);
+  // null is a object while undefined is undefined..
